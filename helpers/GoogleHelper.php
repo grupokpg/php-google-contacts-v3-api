20988d2d39c1f017eb6ca19d3a92bf2bd3bbec4c
Shirleyson Kaisser <skaisser@gmail.com>
2017-12-17 19:49:23 -0200

    Get Logged In User

--- helpers/GoogleHelper.php
+++ helpers/GoogleHelper.php
@@ -48,8 +48,9 @@ abstract class GoogleHelper
         'https://apps-apis.google.com/a/feeds/alias/',
         'https://apps-apis.google.com/a/feeds/user/',*/
         'https://www.google.com/m8/feeds/',
+        'https://www.googleapis.com/auth/userinfo.profile',
         /*'https://www.google.com/m8/feeds/user/',*/
-        ));
+    ));
 
         $client->setClientId($config->clientID);
         $client->setClientSecret($config->clientSecret);
